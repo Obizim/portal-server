6bd703b2de9ee7f802b6b41673b0d851a3983d93
Zim <obizimuzo@gmail.com>
2023-03-23 01:14:48 +0000

    mongoose set

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,26 +1,32 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc Get all jobs
 // @ route GET /api/jobs
-const getJobs = (req, res) => {
+const getJobs = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get jobs" });
-};
+});
 
 // @desc Set a job
 // @ route POST /api/jobs
-const setJobs = (req, res) => {
+const setJobs = asyncHandler(async (req, res) => {
+  if (!req.body.title) {
+    res.status(400);
+    throw new Error("Missing Title");
+  }
   res.status(200).json({ message: "Set Job" });
-};
+});
 
 // @desc Update a job
 // @ route PUT /api/jobs/:id
-const updateJobs = (req, res) => {
+const updateJobs = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete jobs ${req.param.id}` });
-};
+});
 
 // @desc Delete a job
 // @ route DELETE /api/jobs/:id
-const deleteJobs = (req, res) => {
+const deleteJobs = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete jobs ${req.param.id}` });
-};
+});
 
 module.exports = {
   getJobs,
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const dotenv = require("dotenv").config;
+const dotenv = require("dotenv").config();
+const { urlencoded } = require("express");
+const { errHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+connectDB()
 
 const port = process.env.PORT || 4000;
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/jobs", require("./routes/jobsRoutes"));
+app.use(errHandler)
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
